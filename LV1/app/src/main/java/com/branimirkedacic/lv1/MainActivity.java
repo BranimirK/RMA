4f5321efe69442bb57d88903755fba0f7ec772b3
BranimirK <bkedacic@hotmail.com>
2018-03-29 21:51:07 +0200

    Toast 2
    Dodane toast poruke za sve osobe

--- LV1/app/src/main/java/com/branimirkedacic/lv1/MainActivity.java
+++ LV1/app/src/main/java/com/branimirkedacic/lv1/MainActivity.java
@@ -13,6 +13,8 @@ import static android.widget.Toast.makeText;
 public class MainActivity extends AppCompatActivity {
 
     private ImageView ElonSlika;
+    private ImageView BillSlika;
+    private ImageView AlanSlika;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +22,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
        ElonSlika= (ImageView) findViewById(R.id.iv_elon);
+       BillSlika= (ImageView) findViewById(R.id.iv_bill);
+       AlanSlika= (ImageView) findViewById(R.id.iv_alan);
 
        ElonSlika.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                makeText(MainActivity.this, R.string.ElonQuote, Toast.LENGTH_LONG).show();            }
+                makeText(MainActivity.this, R.string.ElonQuote, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        BillSlika.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                makeText(MainActivity.this, R.string.BillQuote, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        AlanSlika.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                makeText(MainActivity.this, R.string.ElonQuote, Toast.LENGTH_LONG).cancel();
+                makeText(MainActivity.this, R.string.AlanQuote, Toast.LENGTH_LONG).show();
+            }
         });
     }
 }
